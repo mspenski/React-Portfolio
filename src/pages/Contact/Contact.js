78f95fa6e09dd29714568eb2e80350b65da965e9
mspenski <44811974+mspenski@users.noreply.github.com>
2019-03-15 14:12:19 -0400

    basic contact setup
    need to do additional styling and actually get form to submit to my email address

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -4,7 +4,38 @@ import "./Contact.css";
 class Contact extends Component {
   render() {
     return (
-      <h1>hello world</h1>
+      <div className="jumbotron">
+        <div class="card">
+
+          <div class="card-header">
+            <h1>Contact</h1>
+          </div>
+
+          <div class="card-body">
+
+            <form>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Name</label>
+                <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Full Name" />
+              </div>
+
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                  placeholder="Enter email" />
+              </div>
+
+              <div class="form-group">
+                <label for="exampleFormControlTextarea1">Message</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+
+          </div>
+        </div>
+
+      </div>
     )
   }
 }
