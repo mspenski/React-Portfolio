8a6e051b97a8dd6da9830ad84a576007f8a410e4
mspenski <44811974+mspenski@users.noreply.github.com>
2019-03-16 13:28:16 -0400

    progress

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import college from './college.jpg'
+import omega from './omegaChat.png'
+import bamazon from './bamazon.jpg'
 import "./Portfolio.css";
 
 class Portfolio extends Component {
   render() {
     return (
-      <h1>hello world</h1>
+      <>
+        <div className="row" id='row1'>
+          <div className="col-9">
+            <img src={college} alt='college' />
+          </div>
+
+          <div className="col-3">
+            <p>
+              text
+            </p>
+          </div>
+
+        </div>
+      </>
     )
   }
 }
