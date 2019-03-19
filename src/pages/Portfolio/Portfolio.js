245de9336c905cff3cc6a4878be8e6348779f1ca
mspenski <44811974+mspenski@users.noreply.github.com>
2019-03-18 21:24:11 -0400

    added text and links

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -10,12 +10,14 @@ class Portfolio extends Component {
       <>
         <div className="row" id='row1'>
           <div className="col-9">
-            <img src={omega} alt='Omega Chat' />
+            <a href="https://rocky-island-31181.herokuapp.com" target="_blank" rel='noopener noreferrer'>
+              <img src={omega} alt='Omega Chat' />
+            </a>
           </div>
           <div className="col-3">
             <h4>Omega Chat</h4>
             <p className='text' id='text1'>
-              text
+              This app was created to simplify and improve communication between members of large Greek Life Organizations. This project was the final project of the bootcamp, and also my brain child, so I was in charge of the entire backend as well as about half of the front end
             </p>
           </div>
         </div>
@@ -26,11 +28,13 @@ class Portfolio extends Component {
           <div className="col-3">
             <h4>COL-lege</h4>
             <p className='text' id='text2'>
-              text
+              COL-edge takes national averages for cost of living and gives that to our users in plain US dollars, while at the same time gives our users the power of camparison: between the average pay associated with a given degree and the place a graduate might choose to live.
             </p>
           </div>
           <div className="col-9">
-            <img src={college} alt='college' />
+            <a href="https://puffroi.herokuapp.com" target="_blank" rel='noopener noreferrer'>
+              <img src={college} alt='college' />
+            </a>
           </div>
 
 
@@ -39,13 +43,17 @@ class Portfolio extends Component {
         <br />
         <div className="row" id='row3'>
           <div className="col-9">
-            <img src={bamazon} alt='bamazon' />
+            <a href="https://drive.google.com/file/d/1coD9LLlMXrMc5LipFRjA0Za5X1jw32RY/view" target="_blank" rel='noopener noreferrer'>
+              <img src={bamazon} alt='bamazon' />
+            </a>
           </div>
 
           <div className="col-3">
             <h4>Bamazon</h4>
             <p className='text' id='text3'>
-              text
+              I created Bamazon as a homework assignment that is meant to function similar to how a site like Amazon would if it were run solely in NodeJS.
+   For this project I used Node, MySQL, Inquirer, & Javascript
+
             </p>
           </div>
 
