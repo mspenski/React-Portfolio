904234556df523bbc13d2833d10fd358304ac87f
mspenski <44811974+mspenski@users.noreply.github.com>
2019-03-20 14:06:39 -0400

    Update Contact.js

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -15,9 +15,9 @@ class Contact extends Component {
 
           <h5><i class="fas fa-at"></i> Email: <a href='mailto:mspenski@gmail.com'>mspenski@gmail.com</a></h5>
           <h5><i class="fas fa-phone"></i> Phone: 336-410-3963</h5>
-          <h5><i className="fab fa-linkedin"></i> <a href="https://www.linkedin.com/in/mitchellspenski">Add me on LinkedIn </a></h5>
-          <h5><i class="fab fa-github"></i> <a href="https://github.com/mspenski">See my other projects on github </a></h5>
-          <h5><i class="fas fa-file-alt"></i> <a href="https://docs.google.com/document/d/1Ti11SM0o5anmzCMxZklh6-G4jMt0Q_XcQkqJICgZrRw/edit?usp=sharing">View my Resume </a></h5>
+          <h5><i className="fab fa-linkedin"></i> <a href="https://www.linkedin.com/in/mitchellspenski" target="_blank" rel='noopener noreferrer'>Add me on LinkedIn </a></h5>
+          <h5><i class="fab fa-github"></i> <a href="https://github.com/mspenski" target="_blank" rel='noopener noreferrer'>See my other projects on github </a></h5>
+          <h5><i class="fas fa-file-alt"></i> <a href="https://docs.google.com/document/d/1Ti11SM0o5anmzCMxZklh6-G4jMt0Q_XcQkqJICgZrRw/edit?usp=sharing" target="_blank" rel='noopener noreferrer'>View my Resume </a></h5>
 
         </div>
       </div>
