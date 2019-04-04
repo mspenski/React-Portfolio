7d2f8983f9e87e2ecf95856663131c0a7c89adee
mspenski <44811974+mspenski@users.noreply.github.com>
2019-04-04 12:52:55 -0400

    It is mobile responsive!

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -9,12 +9,12 @@ class Portfolio extends Component {
     return (
       <>
         <div className="row">
-          <div className="col-9">
+          <div className="col-md-9">
             <a href="https://rocky-island-31181.herokuapp.com" target="_blank" rel='noopener noreferrer'>
               <img src={omega} alt='Omega Chat' />
             </a>
           </div>
-          <div className="col-3">
+          <div className="col-md-3">
             <h4>Omega Chat</h4>
             <p className='text' id='text1'>
               This app was created to simplify and improve communication between members of large Greek Life Organizations. This project was the final project of the bootcamp, and also my brain child, so I was in charge of the entire backend as well as about half of the front end
@@ -27,7 +27,12 @@ class Portfolio extends Component {
         <br />
 
         <div className="row">
-          <div className="col-3">
+          <div className="col-lg-9">
+            <a href="https://puffroi.herokuapp.com" target="_blank" rel='noopener noreferrer'>
+              <img src={college} alt='college' />
+            </a>
+          </div>
+          <div className="col-lg-3">
             <h4>COL-lege</h4>
             <p className='text' id='text2'>
               COL-edge takes national averages for cost of living and gives that to our users in plain US dollars, while at the same time gives our users the power of camparison: between the average pay associated with a given degree and the place a graduate might choose to live.
@@ -35,24 +40,19 @@ class Portfolio extends Component {
             <p className='text' id='text2'><a href="https://github.com/dashinja/COL-edge" target="_blank" rel='noopener noreferrer'>See Project Repo</a></p>
             <p className='text' id='text2'><a href="https://puffroi.herokuapp.com" target="_blank" rel='noopener noreferrer'>See Deployed Project</a></p>
           </div>
-          <div className="col-9">
-            <a href="https://puffroi.herokuapp.com" target="_blank" rel='noopener noreferrer'>
-              <img src={college} alt='college' />
-            </a>
-          </div>
 
 
 
         </div>
         <br />
         <div className="row" id='row3'>
-          <div className="col-9">
+          <div className="col-lg-9">
             <a href="https://drive.google.com/file/d/1coD9LLlMXrMc5LipFRjA0Za5X1jw32RY/view" target="_blank" rel='noopener noreferrer'>
               <img src={bamazon} alt='bamazon' />
             </a>
           </div>
 
-          <div className="col-3">
+          <div className="col-lg-3">
             <h4>Bamazon</h4>
             <p className='text' id='text3'>
               I created Bamazon as a homework assignment that is meant to function similar to how a site like Amazon would if it were run solely in NodeJS.
