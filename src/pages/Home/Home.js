bb95cf716578e9b6d17ad0e449515e2bbcacf3e1
mspenski <44811974+mspenski@users.noreply.github.com>
2020-02-02 13:45:36 -0500

    changes to about, portfolio, and contact

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,26 +8,36 @@ class HomePage extends Component {
       <>
         <div className="container wow fadeIn">
           <div className="card">
-
             <div className="card-header">
               <h1>About Me</h1>
             </div>
-
             <div className="card-body">
-
               <img src={pic} className="auth-image" alt="Mitchell Spenski" />
-
-              <p>Hello, my name is Mitchell Spenski and I am an aspiring Full Stack Web Developer! I graduated from
+              <p> Hello, my name is Mitchell Spenski and I am a Full Stack Developer
+                  currently working as a Software Engineer for PMMC. I graduated from
                   East Carolina University in the summer of 2016 with a B.S. in Health Services Management, and a
                   minor in Business Administration. After graduating from college, I eventually landed a job as an
                   Account Executive with a staffing agency that focused on the IT & Engineering industries. Over my
                   time there, I began to understand the massive scope of the IT industry and learned all about the
                   different positions and what their functions were. It was then that I realized I wanted to pursue
-                  a career that I was truly passionate about. Since then I have dedicated myself to becoming a developer.</p>
+                  a career that I was truly passionate about, which led me to development.
+                </p>
 
-              <p>During my tenure in web development, I have come to truly enjoy both Front End
-                  and Back End work. I have specifically come to love working with Javascript, React, and Python. As well as databases such as Mongo DB and MySQL.
-                  I am seeking an opportunity that will allow me to utilize the vast amount of knowledge that I have gained to contribute to an organization's overall success.</p>
+              <p>During my time as a developer I have come to truly enjoy both web and software development.
+                  In my current role I frequently use C#, AngularJS, .Net, SQL Server, and for the legacy applications, Visual Basic.
+                  I have really enjoyed learning about Object Oriented Programming and am continuously finding ways to refactor old code
+                  to make it more object oriented. I have also become much more adept with SQL; specifically creating data maintenance scripts
+                  and stored procedures. I enjoy learning new languages such as Angular 8, Python, and Java as well as building
+                  on my skillset in the languages that I already know.
+                  I also enjoy reading about best practices and design principles, which is why I
+                  subscribe to Medium for various articles realted to technology and coding.
+              </p>
+              <p>
+                In my free time I really enjoy getting out of the house and spending time with friends and family, watching or going to sporting events,
+                excercising, and reading. I am currently in the process of reading "The Pragmatic Programmer" which I have really enjoyed and found extremely useful.
+                I appreciate you taking the time to get to know more about me and hope you enjoy looking at my project portfolio and Github.
+                There is also a link to my resume on the contact page, or you can view it by clicking <a href="https://docs.google.com/document/d/1Ti11SM0o5anmzCMxZklh6-G4jMt0Q_XcQkqJICgZrRw/edit?usp=sharing" target="_blank" rel='noopener noreferrer'>here</a>.
+              </p>
             </div>
           </div>
         </div>
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -2,12 +2,37 @@ import React, { Component } from 'react';
 import college from './college.jpg'
 import omega from './omegaChat.png'
 import bamazon from './bamazon.jpg'
+import patientPortal from "./patientPortal.png";
 import "./Portfolio.css";
 
 class Portfolio extends Component {
   render() {
     return (
       <>
+        <div className="row">
+          <div className="col-lg-9 wow fadeIn">
+            <a href="http://patientportal.pmmconline.com/dev" target="_blank" rel='noopener noreferrer'>
+              <img src={patientPortal} alt='PMMC Patient Portal' />
+            </a>
+          </div>
+          <div className="col-lg-3 wow fadeIn" data-wow-delay='.5s'>
+            <h4>PMMC Patient Portal</h4>
+            <p className='text' id='text1'>
+              This is one of the products that I am currently responsible for maintaining and improving.
+              Our patient portal is a patient facing product that we make for hospitals around the country that allows
+              patients to input their insurance information and what service they plan to have done and generates an estimate
+              of what their expected cost will be.
+              This specific version is our development branch so this is constantly a work in progress. However,
+              I have included a link to one of our live client's version as well below.
+            </p>
+            <p className='text' id='text1'>
+              <a href="http://patientportal.pmmconline.com/dev/" target="_blank" rel='noopener noreferrer'>Development Version</a> |
+              <a href="http://patientportal.pmmconline.com/tch/" target="_blank" rel='noopener noreferrer'>Live Client Example</a>
+            </p>
+            {/* <p className='text' id='text1'><a href="https://rocky-island-31181.herokuapp.com" target="_blank" rel='noopener noreferrer'>See Deployed Project</a></p> */}
+          </div>
+        </div>
+        <br />
         <div className="row">
           <div className="col-lg-9 wow fadeIn">
             <a href="https://rocky-island-31181.herokuapp.com" target="_blank" rel='noopener noreferrer'>
@@ -23,9 +48,7 @@ class Portfolio extends Component {
             {/* <p className='text' id='text1'><a href="https://rocky-island-31181.herokuapp.com" target="_blank" rel='noopener noreferrer'>See Deployed Project</a></p> */}
           </div>
         </div>
-
         <br />
-
         <div className="row">
           <div className="col-lg-9 wow fadeIn">
             <a href="https://puffroi.herokuapp.com" target="_blank" rel='noopener noreferrer'>
@@ -40,9 +63,6 @@ class Portfolio extends Component {
             <p className='text' id='text2'><a href="https://github.com/dashinja/COL-edge" target="_blank" rel='noopener noreferrer'>Project Repo</a> | <a href="https://puffroi.herokuapp.com" target="_blank" rel='noopener noreferrer'>Deployed Project</a></p>
             {/* <p className='text' id='text2'><a href="https://puffroi.herokuapp.com" target="_blank" rel='noopener noreferrer'>See Deployed Project</a></p> */}
           </div>
-
-
-
         </div>
         <br />
         <div className="row" id='row3'>
